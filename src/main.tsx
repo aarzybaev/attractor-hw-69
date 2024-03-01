ba9111f765b0aacd99c0f0736080a331f5682e49
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-01 14:44:56 +0600

    wrapped by Provider, BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
+import {store} from './app/store';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
