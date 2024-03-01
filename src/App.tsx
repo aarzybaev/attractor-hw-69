d4976ccf54e11bebf4d377267f2a22ff7da3b602
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-01 14:44:06 +0600

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import {Route, Routes} from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Show from './containers/Show/Show';
+
+
 
 const App = () => {
 
   return (
     <>
-      Hello!
+      <Layout>
+        <Routes>
+          <Route path="/" element={(<></>)}/>
+          <Route path="/shows/:id" element={(<Show/>)}/>
+          <Route path="*" element={<h3>Oops! Page not found...</h3>} />
+        </Routes>
+      </Layout>
     </>
   );
 };
